4414ea4f032654d0dc96846b2190578aa0ea4736
Aditya Mohan <aditya530026@gmail.com>
2021-05-14 13:44:43 +0530

    hosted

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ var corsOptions = {
 
 app.use(cors())
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'OPTIONS, GET, POST, PUT, PATCH, DELETE',
-//   )
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-//   next()
-// })
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE',
+  )
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 
 app.use((req, res, next) => {
     bodyParser.json({ limit: '50mb', extended: true })(req, res, (err) => {
